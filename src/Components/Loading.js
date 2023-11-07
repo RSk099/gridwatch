4a0680737bd0535464dadec904ef37f35b5eb9e2
Robb <robert.scoggins@live.co.uk>
2023-11-07 20:39:52 +0000

    refactor: improve Loading component

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,6 +1,21 @@
 import React from "react";
 
+import { CircularProgress } from "@mui/material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Loading = () => {
-  return <>Loading...</>;
+  return (
+    <Container>
+      <CircularProgress color="secondary" />
+    </Container>
+  );
 };
 export default Loading;
